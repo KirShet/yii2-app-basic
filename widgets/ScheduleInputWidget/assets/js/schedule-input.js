6286164101a81ae0bb6efd5951b9ccd4d4c17d91
KirShet <kirshet2000@gmail.com>
2024-12-03 18:44:24 +0600

    feat schedule-input.js and schedule-input.css

--- widgets/ScheduleInputWidget/assets/js/schedule-input.js
+++ widgets/ScheduleInputWidget/assets/js/schedule-input.js
@@ -33,4 +33,72 @@ $(document).ready(function () {
 
     //  кнопка добавления
     container.after(addButton);
+
+
+        // Добавление рабочих часов
+    $('.add-work-time').on('click', function() {
+        $('#work-time-container').append(`
+            <div class="work-time-entry">
+                <select name="schedule[work_time][][day]" required>
+                    <option value="1">Понедельник</option>
+                    <option value="2">Вторник</option>
+                    <option value="3">Среда</option>
+                    <option value="4">Четверг</option>
+                    <option value="5">Пятница</option>
+                    <option value="6">Суббота</option>
+                    <option value="7">Воскресенье</option>
+                </select>
+                <input type="time" name="schedule[work_time][][start_time]" required>
+                <input type="time" name="schedule[work_time][][end_time]" required>
+                <button type="button" class="remove-work-time">Удалить</button>
+            </div>
+        `);
+    });
+
+    // Добавление особенных дней
+    $('.add-special-days').on('click', function() {
+        $('#special-days-container').append(`
+            <div class="special-day-entry">
+                <input type="date" name="schedule[special_days][][date]" required>
+                <input type="time" name="schedule[special_days][][start_time]" required>
+                <input type="time" name="schedule[special_days][][end_time]" required>
+                <button type="button" class="remove-special-day">Удалить</button>
+            </div>
+        `);
+    });
+
+    // Удаление рабочей записи или особенного дня
+    $(document).on('click', '.remove-work-time, .remove-special-day', function() {
+        $(this).parent().remove();
+    });
+
+
+
+
+    $('form').on('submit', function(event) {
+        let isValid = true;
+
+        // Проверка всех временных полей
+        $('input[type="time"]').each(function() {
+            if (!$(this).val()) {
+                isValid = false;
+                $(this).addClass('error');
+            } else {
+                $(this).removeClass('error');
+            }
+        });
+
+        // Если есть незаполненные поля, блокируем отправку формы
+        if (!isValid) {
+            event.preventDefault();
+            alert('Пожалуйста, заполните все поля времени.');
+        }
+    });
+
+    // Удаление класса ошибки при изменении поля
+    $(document).on('input', 'input[type="time"]', function() {
+        if ($(this).val()) {
+            $(this).removeClass('error');
+        }
+    });
 });
